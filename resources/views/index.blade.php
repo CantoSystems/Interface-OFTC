9163ce9a525b14b13ab749c37eb694b2e0bff1fd
Pablo Montoya <jpomprime@gmail.com>
2022-02-01 15:51:09 -0600

    Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('/bootstrap/css/bootstrap.css')}}">
-    <link rel="stylesheet" href="{{ asset('/AdminLTE-master/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('/AdminLTE-master/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
-</head>
-<body>
-    Hola
-</body>
-<!-- jQuery -->
-<script src="{{ asset('/AdminLTE-master/plugins/jquery/jquery.min.js')}}"></script>
-<!-- Bootstrap 4 -->
-<script src="{{ asset('/AdminLTE-master/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('/AdminLTE-master/dist/js/adminlte.min.js')}}"></script>
-<!-- DataTables -->
-<script src="{{ asset('/AdminLTE-master/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{ asset('/AdminLTE-master/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-<script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-<script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-</html>
+@extends('layouts.principal')
+  @section('content') 
+  @endsection
