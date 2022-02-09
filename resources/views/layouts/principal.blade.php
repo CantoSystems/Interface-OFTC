ff710543fb87d51dbb84fa971e71d62ad87c06c4
Pablo Montoya <jpomprime@gmail.com>
2022-02-09 00:07:02 -0600

    Calculo PTU

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -18,15 +18,15 @@
       <nav class="main-header navbar navbar-expand navbar-white navbar-light">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"
-              ><i class="fas fa-bars"></i
-            ></a>
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button">
+              <i class="fas fa-bars"></i>
+            </a>
           </li>
         </ul>
       </nav>
 
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="../../index3.html" class="brand-link">
+        <a href="{{ asset('/inicio') }}" class="brand-link">
           <img src="{{ asset('/AdminLTE-master/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8"/>
           <span class="brand-text font-weight-light">Oftalmocenter</span>
         </a>
@@ -53,7 +53,7 @@
                 </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <a href="{{ route('subirEstudio.index')}}" class="nav-link">
+                    <a href="{{ route('subirEstudio.index') }}" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>Subir archivo Excel</p>
                     </a>
@@ -87,12 +87,12 @@
       </footer>
     </div>
 
-    <script src="{{ asset('/AdminLTE-master/plugins/jquery/jquery.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/dist/js/adminlte.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')}}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('/AdminLTE-master/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
   </body>
 </html>
