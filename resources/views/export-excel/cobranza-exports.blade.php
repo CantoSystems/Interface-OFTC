1718253cb50adf2dc7bcf53570a0f9c7b7db0509
Pablo Montoya <jpomprime@gmail.com>
2022-06-24 12:45:31 -0500

    Consulta de reportes de cobranza

--- resources/views/export-excel/cobranza-exports.blade.php
+++ resources/views/export-excel/cobranza-exports.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <table>
         <thead>
@@ -16,8 +18,10 @@
                 <th>Estudio</th>
                 <th>Tipo de Ojo</th>
                 <th>Doctor</th>
-                <th>Interpretado</th>
                 <th>Transcrito</th>
+                <th>Transcrito Por:</th>
+                <th>Interpretado</th>
+                <th>Interpretado Por:</th>
                 <th>Escaneado</th>
                 <th>Cantidad</th>
             </tr>
@@ -32,14 +36,15 @@
                 <td>{{ $dCobranza->nombretipo_ojo }}</td>
                 <td>{{ $dCobranza->Doctor}}</td>
                 <td>{{ $dCobranza->Transcripcion }}</td>
-                <td>{{ $dCobranza->Interpretacion}}</td>
+                <td>{{ $dCobranza->empleadoTrans }}</td>
+                <td>{{ $dCobranza->Interpretacion }}</td>
+                <td>{{ $dCobranza->empleadoInter }}</td>
                 <td>{{ $dCobranza->Escaneado}}</td>
                 <td>{{ $dCobranza->cantidadCbr}}</td>
-                
             </tr>
-                
             @endforeach
         </tbody>
     </table>
 </body>
+
 </html>
