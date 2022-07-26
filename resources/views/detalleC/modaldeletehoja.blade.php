2aaf5390ec397d3d63b2795ffc88c47ae9d09a43
Pablo Montoya <jpomprime@gmail.com>
2022-07-25 19:40:54 -0500

    Modificaciones Hoja de Consumo

---
+++ resources/views/detalleC/modaldeletehoja.blade.php
@@ -0,0 +1,26 @@
+<div class="modal fade bs-example-modal-sm" tabindex="-1" id="eliminar-hoja" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title modalPersonalizado">Eliminar Hoja de Consumo</h1>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="{{ route('deleteHoja.destroy') }}" method="POST">
+                <div class="modal-body">
+                    @method('DELETE')
+                    @csrf
+                    <p>¿Estás seguro que deseas eliminar la hoja de consumo de <b id="modalDoctor"
+                            name="modalDoctor"></b>?</p>
+                    <input type="hidden" id="idHojaConsumo" name="idHojaConsumo">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" data-dismiss="modal"
+                        class="btn btn-outline-secondary btn-xs">Cancelar</button>
+                    <button type="submit" class="btn btn-outline-danger btn-xs">Continuar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
