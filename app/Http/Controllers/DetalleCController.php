651fb5f9d0bbe500c649e115c262a493b6752af6
Elizabeth <anely_lyzzye@hotmail.com>
2022-09-28 13:03:04 -0500

    prueba

--- app/Http/Controllers/DetalleCController.php
+++ app/Http/Controllers/DetalleCController.php
@@ -585,8 +585,10 @@ class DetalleCController extends Controller{
     }
 
     public function deleteHoja($id_detalle){
+        
         $deleteHoja = DB::table('detalle_adicional')->where('id_detalleConsumo_FK','=',$id_detalle)->delete();
         $deleteHoja2 = DB::table('detalle_consumos')->where('id','=',$id_detalle)->delete();
+        
         $doctores = Doctor::where('id','!=',1)->get();
         $tipoPaciente = TipoPaciente::all();
         
