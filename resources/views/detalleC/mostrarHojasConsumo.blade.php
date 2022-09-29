a4b7f7c294da1a3df2202f59e0dfec92b3bd9f52
Pablo Montoya <jpomprime@gmail.com>
2022-09-29 11:15:18 -0500

    Correciones

--- resources/views/detalleC/mostrarHojasConsumo.blade.php
+++ resources/views/detalleC/mostrarHojasConsumo.blade.php
@@ -124,7 +124,7 @@
                                         <input type="text" name="doctorHoja" id="doctorHoja"
                                             value="{{ $hojas->Doctor }}">
                                         <a>
-                                            <button type="button" id="btnDlt" name="btnDlt" class="btn btn-danger btn-sm">
+                                            <button type="button" id="btnDlt" name="btnDlt" onClick="borrarHoja();" class="btn btn-danger btn-sm">
                                                 <i class="far fa-trash-alt"></i>
                                             </button>
                                         </a>
