bdd1fde7f372648558730bb32a00082e6b7cdff9
pablomontoya <jpomprime@gmail.com>
2023-05-10 12:31:02 -0600

    Correciones inserciones datos cobranza_porcentaje y cobranza_total

--- resources/views/layouts/plantillaEstudiosTemps.blade.php
+++ resources/views/layouts/plantillaEstudiosTemps.blade.php
@@ -289,10 +289,12 @@
     }
 
     $(document).ready(function() {
-        countUtilidades = $('#countUtilidades').val();
+        /*countUtilidades = $('#countUtilidades').val();
         if (countUtilidades > 0) {
-
-        }
+            $(".chckUtilidad").prop("disabled", true);
+        } else {
+            alert("No se ha pagado");
+        }*/
 
         $('.transRdS').click(function() {
             $('#drTransc').attr("disabled", false);
